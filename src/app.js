d1ba0fe8a50dbdb0658e52ec5004831e8d85bda8
ishapatel1612 <ishapatel1612@gmail.com>
2023-05-29 19:30:58 +0100

    Showing current weather values
    temp / humidity / wind / weather description

---
+++ src/app.js
@@ -0,0 +1,25 @@
+let apiKey = `6c24b098b2bd19340ed515f93a30a1a9`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemp);
+
+function displayTemp(response) {
+  console.log(response.data);
+
+  let temperatureElement = document.querySelector("#temp");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+
+  let weatherDescriptionElement = document.querySelector(
+    "#weather-description"
+  );
+  weatherDescriptionElement.innerHTML = response.data.weather[0].main;
+
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = response.data.main.humidity;
+
+  let windSpeed = document.querySelector("#wind-speed");
+  windSpeed.innerHTML = Math.round(response.data.wind.speed);
+}
