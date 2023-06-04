693d1494bc5df9404ae49bf449165bd2ee991824
ishapatel1612 <ishapatel1612@gmail.com>
2023-06-04 22:05:30 +0100

    Successfully integrated forecast API

--- src/app.js
+++ src/app.js
@@ -22,14 +22,14 @@ function formatDate(timestamp) {
 }
 
 function getForecast(coordinates) {
-  let apiKey = `6c24b098b2bd19340ed515f93a30a1a9`;
+  let apiKey = `0efb4fc16a9ed98dc0b3aafd8491d6ad`;
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
 
-  console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data);
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row mb-3">`;
 
